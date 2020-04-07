6d5027df832fdf0ce1be298d5ccf6781edec3644
Max Ageev <ageev.maks2014@gmail.com>
2020-04-07 11:22:08 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,39 @@ package hw02_unpack_string //nolint:golint,stylecheck
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here
-	return "", nil
+func Unpack(s string) (string, error) {
+	var out strings.Builder
+	var c rune
+	out.Grow(len(s))
+	var flagEscape bool
+	for index, value := range s {
+		switch {
+		case value == '\\' && !flagEscape:
+			flagEscape = true
+		case flagEscape:
+			out.WriteRune(value)
+			flagEscape, c = false, value
+		case unicode.IsNumber(value):
+			if c != 0 {
+				for a := 1; a < int(value-'0'); a++ {
+					out.WriteRune(c)
+				}
+				c = 0
+			} else {
+				return "", errors.New("invalid string")
+			}
+		default:
+			if (len(s) > (index+1) && s[index+1] != '0') || (len(s) == (index + 1)) {
+				out.WriteRune(value)
+				c = value
+			}
+		}
+	}
+	return out.String(), nil
 }
